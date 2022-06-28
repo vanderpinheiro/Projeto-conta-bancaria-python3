d696b0c0facf8fbe108c15c5098438fec60fc10b
VANDER PINHEIRO ALVES OLIVEIRA <vander.cp2@gmail.com>
2022-06-28 12:01:29 -0300

    inicio_projeto_bank
    Projeto de conta bancária em POO - Python

---
+++ projeto_agencia_bancaria/cc.py
@@ -0,0 +1,24 @@
+from conta import Conta
+
+class ContaCorrente(Conta):
+    def __init__(self,agencia,conta,saldo,limite=100):
+        super().__init__(agencia,conta,saldo)
+        self._limite = limite
+    def limite(self):
+        return self._limite
+    def sacar(self,valor):
+        if not isinstance(valor,(int,float)):
+            print('O valor a ser sacado precisa ser um número.')
+            return
+        if self.saldo + self.limite < valor:
+            print(f'Seu salado é R${self.saldo}, você não pode sacar R${valor}.\n Saldo insuficiente')
+            return
+        self.saldo -= valor
+        self.detalhes()
+
+c1= ContaCorrente(212,23213,100)
+
+c1.detalhes()
+c1.sacar(100)
+c1.sacar(100)
+c1.sacar(100)
---
+++ projeto_agencia_bancaria/cp.py
@@ -0,0 +1,18 @@
+from conta import Conta
+
+class ContaPoupanca(Conta):
+    def sacar(self,valor):
+        if not isinstance(valor,(int,float)):
+            print('O valor a ser sacado precisa ser um número')
+            return
+        if self.saldo < valor:
+            print(f'Seu salado é R${self.saldo}, você não pode sacar R${valor}.\n Saldo insuficiente')
+            return
+        self.saldo -= valor
+        print(f'Você sacou {valor}')
+        self.detalhes()
+
+
+c = ContaPoupanca(6897,87653,10)
+c.detalhes()
+c.sacar(20)
